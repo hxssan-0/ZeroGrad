342a3c8f53ffb8734c1b152cde33ee6727eaef03
hxssan-0 <hassanriz2006@gmail.com>
2026-06-26 14:31:01 +0500

    designed the Scalar class

--- include/zerograd/scalar.h
+++ include/zerograd/scalar.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include <functional>
+#include <memory>
+#include <utility>
+
 namespace zerograd {
-    struct Scalar {
-        float data;
-        Scalar(float val) : data(val) {}
+
+    class Scalar {
+    private:
+        std::vector<std::shared_ptr<Scalar>> _children;
+        std::string _op;
+        std::function<void()> _backward = [](){};
+
+    public:
+        float data{};
+        float grad{};
+
+        explicit Scalar(float data,
+                        std::vector<std::shared_ptr<Scalar>> children = {},
+                        std::string op = "")
+            : data(data), _children(std::move(children)), _op(std::move(op)) {}
     };
 }
